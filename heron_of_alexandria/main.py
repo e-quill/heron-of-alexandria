e05b32c234cd619fd24f6331eeab1b1e8b0837a2
e-quill <128042639+e-quill@users.noreply.github.com>
2024-01-24 23:40:29 +0530

    Add input fields and position labels

--- heron_of_alexandria/main.py
+++ heron_of_alexandria/main.py
@@ -9,10 +9,24 @@ mw.resizable(False, False)
 background_image = tkinter.PhotoImage(file = "heron.png")
 
 image_label = tkinter.Label(mw, image = background_image) 
-image_label.place(x = -300, y = 0) 
+image_label.place(x = -250, y = 0) 
 
-base = tkinter.Label(mw, text='Base',font=("Times", 24 )).grid(column=1)
-height = tkinter.Label(mw, text='Height', font=("Times", 24 )).grid(column=0)
+base = tkinter.Label(mw, text='Base', bg="#ffffff",font=("Kozuka Mincho Pro R", 13 )).grid(column=0, padx=(400, 10), pady=(150,10))
+height = tkinter.Label(mw, text='Height', bg="#ffffff", font=("Kozuka Mincho Pro R", 13 )).grid(row=1, column=0, padx=(400, 10), pady=(10,10))
+
+e1 = tkinter.Entry(mw)
+e2 = tkinter.Entry(mw)
+
+e1.grid(row=0, column=1, pady=(150,10))
+e2.grid(row=1, column=1)
+
+print(e1.get())
+
+
+
+
+
+
 
 
 
