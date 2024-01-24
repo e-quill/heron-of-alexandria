602b286368809ba07224d18d0b6c64bc7ac56c3a
e-quill <128042639+e-quill@users.noreply.github.com>
2024-01-24 14:11:37 +0530

    Add height, base labels remove fullscreen

--- heron_of_alexandria/main.py
+++ heron_of_alexandria/main.py
@@ -1,13 +1,20 @@
 import tkinter 
 
-mw = tkinter.Tk()
+mw = tkinter.Tk() #initialize main window
 
 mw.title("Heron of Alexandria")
 mw.geometry("700x600")
+mw.resizable(False, False)
 
-bg = tkinter.PhotoImage(file = "heron.png")
+background_image = tkinter.PhotoImage(file = "heron.png")
 
-label1 = tkinter.Label(mw, image = bg) 
-label1.place(x = -300, y = 0) 
+image_label = tkinter.Label(mw, image = background_image) 
+image_label.place(x = -300, y = 0) 
+
+base = tkinter.Label(mw, text='Base',font=("Times", 24 )).grid(column=1)
+height = tkinter.Label(mw, text='Height', font=("Times", 24 )).grid(column=0)
+
+
+
 
 mw.mainloop()
