101e690662816ce9b3047a8a6763687edf367ee8
e-quill <128042639+e-quill@users.noreply.github.com>
2024-01-24 12:03:51 +0530

    Add background image and set dimensions

--- heron_of_alexandria/main.py
+++ heron_of_alexandria/main.py
@@ -1,5 +1,13 @@
-import tkinter
+import tkinter 
 
 mw = tkinter.Tk()
+
+mw.title("Heron of Alexandria")
+mw.geometry("700x600")
+
+bg = tkinter.PhotoImage(file = "heron.png")
+
+label1 = tkinter.Label(mw, image = bg) 
+label1.place(x = -300, y = 0) 
 
 mw.mainloop()
