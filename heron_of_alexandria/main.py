fcd53cadebabd9abb07388964deaac02013d0ab9
e-quill <128042639+e-quill@users.noreply.github.com>
2024-01-25 13:42:08 +0530

    Create dialog box for output

--- heron_of_alexandria/main.py
+++ heron_of_alexandria/main.py
@@ -1,8 +1,10 @@
 import tkinter 
-
-def calc_area():
-    print(int(1/2 * int(base_entry.get()) * int(height_entry.get())))
-
+from tkinter import messagebox
+    
+def show_dialog():
+  area = int(1/2 * int(base_entry.get()) * int(height_entry.get()))
+  messagebox.showinfo("Area",f"The Area of the Triangle is {area} units.")
+  
 mw = tkinter.Tk() #initialize main window
 
 mw.title("Heron of Alexandria")
@@ -11,8 +13,8 @@ mw.resizable(False, False)
 
 background_image = tkinter.PhotoImage(file = "heron.png")
 
-image_label = tkinter.Label(mw, image = background_image) 
-image_label.place(x = -250, y = 0) 
+background_image_label = tkinter.Label(mw, image = background_image) 
+background_image_label.place(x = -250, y = 0) 
 
 base = tkinter.Label(mw, text='Base:', bg="#ffffff",font=("Kozuka Mincho Pro R", 13 )).grid(column=0, padx=(400, 10), pady=(150,10))
 height = tkinter.Label(mw, text='Height:', bg="#ffffff", font=("Kozuka Mincho Pro R", 13 )).grid(row=1, column=0, padx=(400, 10), pady=(10,10))
@@ -23,7 +25,9 @@ height_entry = tkinter.Entry(mw)
 base_entry.grid(row=0, column=1, pady=(150,10))
 height_entry.grid(row=1, column=1)
 
-calculate_button = tkinter.Button(mw, text='Calculate', command=calc_area).grid(row=3, column=1, sticky=tkinter.W, pady=4)
+calculate_button = tkinter.Button(mw, text='Calculate Area', command=show_dialog).grid(row=3, column=1, sticky=tkinter.W, pady=4)
+
+
 
 
 mw.mainloop()
