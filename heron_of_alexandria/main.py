c74f55c4103b94f8f2a8d68046a941cb69e7665e
Aadil Varsh <advrxh@duck.com>
2024-01-25 15:37:39 +0530

    Update main.py

--- heron_of_alexandria/main.py
+++ heron_of_alexandria/main.py
@@ -2,9 +2,18 @@ import tkinter
 import logic
 from tkinter import messagebox
 
+def clear():
+    base_entry.delete(0, "end")
+    height_entry.delete(0, "end")
+
 def show_dialog():
-  area = logic.area_of_triangle(int(base_entry.get()),int(height_entry.get()))
-  messagebox.showinfo("Area",f"The Area of the Triangle is {area} units.")
+    area = logic.area_of_triangle(float(base_entry.get()),float(height_entry.get()))
+
+    if area is not None:
+        messagebox.showinfo("Area",f"The Area of the Triangle is {area} units.")
+        clear()
+    else:
+        messagebox.showinfo("Invalid input.")
   
 mw = tkinter.Tk() #initialize main window
 
